7c71eadc55482fc14886b9b0b8a5a234375e3ff9
Shayan Amani <7shayan7@gmail.com>
2020-08-25 23:35:44 -0400

    Added the headless driver option

--- driver.py
+++ driver.py
@@ -1,13 +1,12 @@
-# Author: Stan Fortoński
-# Date: 02.05.2020
-# Driver With Geo Location
-
 from selenium import webdriver
+
 
 def getDriver():
     geoEnable = webdriver.FirefoxOptions()
     geoEnable.set_preference('geo.enabled', True)
-    geoEnable.set_preference('geo.provider.use_corelocation', True);
-    geoEnable.set_preference('geo.prompt.testing', True);
-    geoEnable.set_preference('geo.prompt.testing.allow', True);
+    geoEnable.set_preference('geo.provider.use_corelocation', True)
+    geoEnable.set_preference('geo.prompt.testing', True)
+    geoEnable.set_preference('geo.prompt.testing.allow', True)
+    # make a headless instance of Gecko driver
+    # geoEnable.add_argument('--headless')
     return webdriver.Firefox(options=geoEnable)
